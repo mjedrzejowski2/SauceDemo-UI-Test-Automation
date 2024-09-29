9145270ec69f036dbfd4d01304b8c44ff7522b2f
mjedrzejowski2 <mjedrzejowski00@gmail.com>
2024-09-29 15:22:45 +0200

    v01-test_login_page

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,13 +5,15 @@ def test_login_page_loads_correctly(browser):
     login_page = LoginPage(browser)
     login_page.load()
 
+    #* sluzy do odpakowania
     assert browser.find_element(*LoginPage.USERNAME_INPUT).is_displayed()
     assert browser.find_element(*LoginPage.PASSWORD_INPUT).is_displayed()
     assert browser.find_element(*LoginPage.LOGIN_BUTTON).is_displayed()
 
 
+def test_login_page(browser):
+    login_page = LoginPage(browser)
+    login_page.load()
+    login_page.login("standard_user", "secret_sauce")
 
-
-# def test_login_page(browser):
-#     login_page = LoginPage(browser)
-#     login_page.load()
+    assert "inventory" in browser.current_url
