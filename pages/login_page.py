47eb6505a016e21fda422682fe01012da61b5f4d
mjedrzejowski2 <mjedrzejowski00@gmail.com>
2024-09-30 17:14:02 +0200

    test_login - standard and locked out user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 class LoginPage:
 
@@ -6,6 +7,7 @@ class LoginPage:
     USERNAME_INPUT = (By.ID, "user-name")
     PASSWORD_INPUT = (By.ID, "password")
     LOGIN_BUTTON = (By.ID, "login-button")
+    ERROR_INFO = (By.XPATH, "//*[@id='login_button_container']/div/form")
 
     def __init__(self, driver):
         self.driver = driver
@@ -17,3 +19,18 @@ class LoginPage:
         self.driver.find_element(*self.USERNAME_INPUT).send_keys(username)
         self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
         self.driver.find_element(*self.LOGIN_BUTTON).click()
+
+    def error_exists(self):
+        return len(self.driver.find_elements(*self.ERROR_INFO)) > 0
+
+    def get_error_communicate(self):
+        if self.error_exists():
+            return self.driver.find_element(*self.ERROR_INFO).text
+        
+        return None
+
+if __name__ == '__main__':
+    x = LoginPage(webdriver.Chrome())
+    x.load()
+    x.login("standard_user", "secret_sauce")
+    x.get_error_communicate()
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,7 @@
 import pytest
 from pages.login_page import LoginPage
 
+@pytest.mark.skip
 def test_login_page_loads_correctly(browser):
     login_page = LoginPage(browser)
     login_page.load()
@@ -11,9 +12,28 @@ def test_login_page_loads_correctly(browser):
     assert browser.find_element(*LoginPage.LOGIN_BUTTON).is_displayed()
 
 
-def test_login_page(browser):
+# Standard User: 1 przypadek testowy (poprawne logowanie).
+# Locked Out User: 1 przypadek testowy (komunikat o zablokowanym koncie).
+# Problem User: 1 przypadek testowy (logowanie z problemami).
+# Performance Glitch User: 1 przypadek testowy (logowanie, ale wolne działanie).
+# Error User: 1 przypadek testowy (błąd przy logowaniu).
+# Visual User: 1 przypadek testowy (poprawne logowanie + sprawdzenie UI).
+
+@pytest.mark.login
+@pytest.mark.standard_user
+def test_login_standard_user(browser):
     login_page = LoginPage(browser)
     login_page.load()
     login_page.login("standard_user", "secret_sauce")
 
-    assert "inventory" in browser.current_url
+    assert 'inventory' in browser.current_url, f"Expected 'inventory' in URL, got {browser.current_url}"
+
+
+@pytest.mark.login
+@pytest.mark.locked_out_user
+def test_login_locked_out_user(browser):
+    login_page = LoginPage(browser)
+    login_page.load()
+    login_page.login("locked_out_user", "secret_sauce")
+
+    assert login_page.get_error_communicate() == 'Epic sadface: Sorry, this user has been locked out.'
